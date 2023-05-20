2e1dd1c8c7f0f3986edb4ba9fd1ad30695453d6d
oza-c <matthes@schlichte.dev>
2023-05-20 13:17:46 +0200

    sync commit

--- target/scripts/startup/setup.d/caddy.sh
+++ target/scripts/startup/setup.d/caddy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Just a helper to prepend the log messages with `(Postfix setup)` so
+# users know exactly where the message originated from.
+#
+# @param ${1} = log level
+# @param ${2} = message
+function __caddy__log { _log "${1:-}" "(Caddy setup) ${2:-}" ; }
+
 function _setup_caddy
 {
       _log 'debug' 'Setting up Caddy WebServer'
+      mkdir -p /etc/caddy
       #WIP
       
 }
